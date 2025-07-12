410c4c14c43b3b6216ad3231bdcf116ae1784e78
Chase Thompson <146985322+chaseltb@users.noreply.github.com>
2025-07-11 23:24:42 -0400

    Add rounded corners and technology carousel

--- src/main.js
+++ src/main.js
@@ -32,16 +32,98 @@ setupTestimonials()
 setupFAQ()
 setupCTA()
 
-// Setup trusted by section
-document.querySelector('#trusted-by').innerHTML = `
+// Setup technology carousel section
+document.querySelector('#tech-carousel').innerHTML = `
   <div class="container">
-    <p class="trusted-text">Trusted by developers at</p>
-    <div class="trusted-logos">
-      <div class="logo-item">Startups</div>
-      <div class="logo-item">Scale-ups</div>
-      <div class="logo-item">Enterprises</div>
-      <div class="logo-item">Agencies</div>
-      <div class="logo-item">Consultancies</div>
+    <p class="tech-text">Works with your favorite technologies</p>
+    <div class="tech-carousel-container">
+      <div class="tech-carousel">
+        <div class="tech-item" data-tech="react">
+          <div class="tech-icon"><i class="ph ph-atom"></i></div>
+          <div class="tech-name">React</div>
+        </div>
+        <div class="tech-item" data-tech="vue">
+          <div class="tech-icon"><i class="ph ph-triangle"></i></div>
+          <div class="tech-name">Vue</div>
+        </div>
+        <div class="tech-item" data-tech="angular">
+          <div class="tech-icon"><i class="ph ph-shield"></i></div>
+          <div class="tech-name">Angular</div>
+        </div>
+        <div class="tech-item" data-tech="nodejs">
+          <div class="tech-icon"><i class="ph ph-leaf"></i></div>
+          <div class="tech-name">Node.js</div>
+        </div>
+        <div class="tech-item" data-tech="python">
+          <div class="tech-icon"><i class="ph ph-snake"></i></div>
+          <div class="tech-name">Python</div>
+        </div>
+        <div class="tech-item" data-tech="java">
+          <div class="tech-icon"><i class="ph ph-coffee"></i></div>
+          <div class="tech-name">Java</div>
+        </div>
+        <div class="tech-item" data-tech="postgresql">
+          <div class="tech-icon"><i class="ph ph-database"></i></div>
+          <div class="tech-name">PostgreSQL</div>
+        </div>
+        <div class="tech-item" data-tech="mongodb">
+          <div class="tech-icon"><i class="ph ph-leaf"></i></div>
+          <div class="tech-name">MongoDB</div>
+        </div>
+        <div class="tech-item" data-tech="redis">
+          <div class="tech-icon"><i class="ph ph-lightning"></i></div>
+          <div class="tech-name">Redis</div>
+        </div>
+        <div class="tech-item" data-tech="docker">
+          <div class="tech-icon"><i class="ph ph-package"></i></div>
+          <div class="tech-name">Docker</div>
+        </div>
+        <div class="tech-item" data-tech="kubernetes">
+          <div class="tech-icon"><i class="ph ph-circles-three"></i></div>
+          <div class="tech-name">Kubernetes</div>
+        </div>
+        <div class="tech-item" data-tech="aws">
+          <div class="tech-icon"><i class="ph ph-cloud"></i></div>
+          <div class="tech-name">AWS</div>
+        </div>
+        <div class="tech-item" data-tech="azure">
+          <div class="tech-icon"><i class="ph ph-cloud-check"></i></div>
+          <div class="tech-name">Azure</div>
+        </div>
+        <div class="tech-item" data-tech="gcp">
+          <div class="tech-icon"><i class="ph ph-cloud-arrow-up"></i></div>
+          <div class="tech-name">Google Cloud</div>
+        </div>
+        <!-- Duplicate for seamless loop -->
+        <div class="tech-item" data-tech="react">
+          <div class="tech-icon"><i class="ph ph-atom"></i></div>
+          <div class="tech-name">React</div>
+        </div>
+        <div class="tech-item" data-tech="vue">
+          <div class="tech-icon"><i class="ph ph-triangle"></i></div>
+          <div class="tech-name">Vue</div>
+        </div>
+        <div class="tech-item" data-tech="angular">
+          <div class="tech-icon"><i class="ph ph-shield"></i></div>
+          <div class="tech-name">Angular</div>
+        </div>
+        <div class="tech-item" data-tech="nodejs">
+          <div class="tech-icon"><i class="ph ph-leaf"></i></div>
+          <div class="tech-name">Node.js</div>
+        </div>
+        <div class="tech-item" data-tech="python">
+          <div class="tech-icon"><i class="ph ph-snake"></i></div>
+          <div class="tech-name">Python</div>
+        </div>
+        <div class="tech-item" data-tech="java">
+          <div class="tech-icon"><i class="ph ph-coffee"></i></div>
+          <div class="tech-name">Java</div>
+        </div>
+        <div class="tech-item" data-tech="postgresql">
+          <div class="tech-icon"><i class="ph ph-database"></i></div>
+          <div class="tech-name">PostgreSQL</div>
+        </div>
+      </div>
     </div>
   </div>
 `
