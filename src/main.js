6cf3f7da22dabe9840c01f8044ce06f4a3a28158
Chase Thompson <146985322+chaseltb@users.noreply.github.com>
2025-07-13 23:55:46 -0400

    Remove invalid HTML from main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 import './style.css'
 import { setupNavigation } from './components/navigation.js'
 import { setupHero } from './components/hero.js'
+import { setupTechCarousel } from './components/techCarousel.js'
 import { setupFeatures } from './components/features.js'
-setupFooter()
 import { setupProcess } from './components/process.js'
-          <a href="#hero">Home</a>
-        </div>
-        <div class="footer-column">
-          <h4>Support</h4>
-          <a href="#faq">Help Center</a>
-          <a href="#cta">Contact</a>
-          <a href="#testimonials">Reviews</a>
-        </div>
-      </div>
-    </div>
-    <div class="footer-bottom">
-      <p>&copy; 2025 SystemSketch. All rights reserved.</p>
-    </div>
-  </div>
+import { setupTestimonials } from './components/testimonials.js'
+import { setupFAQ } from './components/faq.js'
+import { setupCTA } from './components/cta.js'
+import { setupFooter } from './components/footer.js'
+
+// Initialize all components
+setupNavigation()
+setupHero()
+setupTechCarousel()
+setupFeatures()
+setupProcess()
+setupTestimonials()
+setupFAQ()
+setupCTA()
+setupFooter()
